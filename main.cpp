abaf0c279058a26ba99063c67d918fe7300ef802
Otherfile <sunny.suren122@gmail.com>
2025-10-05 09:42:08 -0600

    Backup

--- main.cpp
+++ main.cpp
@@ -1,18 +1,66 @@
-#include<stdio.h>
-#include<stdlb.h>
+#include<iostream>
+#include<vector>
+#include<fstream>
+#include"pixel.h"
+
+
+void average_colors(std::vector<Pixel> &pixel_list){
+
+	int size = pixel_list.size();
+	
+	float r_avg = 0;
+	float g_avg = 0;
+	float b_avg = 0;
+
+	for (int i = 0; i < size; i++){
+
+
+		r_avg += pixel_list[i].r; 
+		g_avg += pixel_list[i].g;
+		b_avg += pixel_list[i].b; 
+	}
+
+	r_avg /= size;
+	g_avg /= size;
+	b_avg /= size;
+	
+	std::cout << "- - - - - - Average of R, G, and B - - - - - -" << std::endl;
+
+	std::cout << "R Average: " << r_avg << std::endl << "G Average: " << g_avg << std::endl << "B Average: " << b_avg << std::endl;
+
+}
+
 
 Pixel make_pixel(std::string& line){
 	
+	std::cout << "Test" << std::endl;
+
 	Pixel pixel;
 	int pos;
 	pos = line.find(',');
+
 	pixel.x = stoi(line.substr(0, pos));
-	
 	line = line.substr(pos+1);
 
-	// repeat this for y, r, g
+	pos = line.find(',');
+       	pixel.y = stoi(line.substr(0, pos));
+	line = line.substr(pos+1);
+
+	pos = line.find(',');
+       	pixel.r = stof(line.substr(0, pos));
+	line = line.substr(pos+1);
+
+	pos = line.find(',');
+	pixel.g = stof(line.substr(0, pos));
+	line = line.substr(pos+1);
 
 	pixel.b = stof(line);
+	
+	std::cout << pixel.x << std::endl;
+	std::cout << pixel.y << std::endl;
+	std::cout << pixel.r << std::endl;
+	std::cout << pixel.g << std::endl;
+	std::cout << pixel.b << std::endl;
 
 	return pixel;
 
@@ -20,16 +68,29 @@ Pixel make_pixel(std::string& line){
 }
 
 void flip_vertically(std::vector<Pixel>&pixel_list){
+	
+	int size = pixel_list.size();
 
-	int max - 6;
-
-	for (int i = 0; i < pixel_list.size(); i++)
+	for (int i = 0; i < size; i++)
 	{
 
-		pixel_list[i].y = (max - 1) - (pixel_lisst[i].y);
+		pixel_list[i].y = (size - 1) - (pixel_list[i].y);
 
 	}
 
+	std::ofstream file("flipped.dat");
+
+    	if (!file.is_open()) {
+        	std::cout << "Could not open file for writing!\n";
+        	return;
+   	 }
+
+
+	for (const auto& p : pixel_list) {
+    		file << p.x << "," << p.y << "," << p.r << "," << p.g << "," << p.b << "\n";
+	}
+
+	file.close();
 }
 
 
@@ -37,21 +98,20 @@ int main(int argc, char* argv[]){
 
 	std::vector<Pixel>pixel_list;
 	std::string line;
-	std::ifstream file(" ");
+	std::ifstream file(argv[1]);
 
 	if (file.is_open()){ 
 		
-		std::cout << "Opened file " << file << std::endl;
+		std::cout << "Opened file " << argv[1] << std::endl;
 		
 		while(getline(file, line)){
 		
-			std::cout << line << std::endl;
-			pixel.list.push_back(make_pixel(line));
-
+			std::cout << argv[1] << std::endl;
+			pixel_list.push_back(make_pixel(line));
 		}
 
 		file.close();
-
+			
 	}
 
 	else {
@@ -59,8 +119,16 @@ int main(int argc, char* argv[]){
 		std::cout << "No source file found... Terminating..." << std::endl;
 		return 1;
 	}
+
+
 	
+	flip_vertically(pixel_list);
+	
+	average_colors(pixel_list);
 
-
+	for (int i = 0; i < pixel_list.size(); i++) {
+    		std::cout << "x: " << pixel_list[i].x << ", y: " << pixel_list[i].y << ", r: " << pixel_list[i].r << ", g: " << pixel_list[i].g << ", b: " << pixel_list[i].b << std::endl;
+	}
+	std::cout << "Number of pixels loaded: " << pixel_list.size() << std::endl;
 
 }
